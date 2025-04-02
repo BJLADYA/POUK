aed3490a9637f90b6ae4b7d8ed4f22140f27ac56
BJLADYA <vl@dislav-medvedev.ru>
2025-04-02 17:18:44 +0300

    Added alg2

--- src/lab2/src/alg2.cpp
+++ src/lab2/src/alg2.cpp
@@ -5,29 +5,34 @@
 
 using namespace std::chrono_literals;
 
+double right_distance = 0.0;
 bool obstacle = false;
+const double WALL_DISTANCE = 0.7;
 rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub;
 rclcpp::Node::SharedPtr node;
 
 void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
-	RCLCPP_DEBUG(node->get_logger(), "Laser msg: %f", msg->scan_time);
+  RCLCPP_DEBUG(node->get_logger(), "Laser msg: %f", msg->scan_time);
+  right_distance = msg->ranges[msg->ranges.size() / 4];
+  obstacle = false;
+  const int fov = 90;
+  const double kMinRange = 0.5;
 
-	obstacle = false;
-	const int fov = 90;
-	const double kMinRange = 0.5;
-
-	int dAngle = (180 - fov) / 2;
-
-	for (size_t i = dAngle; i < msg->ranges.size() - dAngle; i++)
-	{
-		if (msg->ranges[i] < kMinRange)
-		{
-			obstacle = true;
-			RCLCPP_WARN(node->get_logger(), "OBSTACLE!!!");
-			break;
-		}
-	}
+  int dAngle = (180 - fov) / 2;
+  if (std::isinf(right_distance) || std::isnan(right_distance))
+  {
+    right_distance = msg->range_max;
+  }
+  for (size_t i = dAngle; i < msg->ranges.size() - dAngle; i++)
+  {
+    if (msg->ranges[i] < kMinRange)
+    {
+      obstacle = true;
+      RCLCPP_WARN(node->get_logger(), "OBSTACLE!!!");
+      break;
+    }
+  }
 }
 
 
@@ -38,52 +43,60 @@ void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
  */
 void poseCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-	RCLCPP_DEBUG(node->get_logger(),
-		"Pose msg: x = %f y = %f theta = %f",
-		msg->pose.pose.position.x,
-		msg->pose.pose.position.y,
-		2 * atan2(msg->pose.pose.orientation.z, msg->pose.pose.orientation.w)
-	);
+  RCLCPP_DEBUG(node->get_logger(),
+    "Pose msg: x = %f y = %f theta = %f",
+    msg->pose.pose.position.x,
+    msg->pose.pose.position.y,
+    2 * atan2(msg->pose.pose.orientation.z, msg->pose.pose.orientation.w)
+  );
 }
 
 void timerCallback()
-{  
-	static int counter = 0;
-	counter++;
-	RCLCPP_DEBUG(node->get_logger(), "on timer %d", counter);
-	geometry_msgs::msg::Twist cmd;
+{
+  static int counter = 0;
+  counter++;
+  RCLCPP_DEBUG(node->get_logger(), "on timer %d", counter);
+  geometry_msgs::msg::Twist cmd;
 
-    if (!obstacle) {
-		RCLCPP_INFO(node->get_logger(), "go forward");
-		cmd.linear.x = 0.5;
-		cmd.angular.z = 0;
-	} else {
-		RCLCPP_INFO(node->get_logger(), "go right");
-		cmd.linear.x = 0;
-		cmd.angular.z = -0.5;
-	}
+  if (!obstacle) {
+    if (right_distance == WALL_DISTANCE){
+      RCLCPP_INFO(node->get_logger(), "go forward");
+      cmd.linear.x = 1;
+      cmd.angular.z = 0;
+    }
+    else{
+      RCLCPP_INFO(node->get_logger(), "turning");
+      cmd.linear.x = 1;
+      cmd.angular.z = WALL_DISTANCE - right_distance;
+    }
+  }
+  else {
+    RCLCPP_INFO(node->get_logger(), "go left");
+    cmd.linear.x = 0;
+    cmd.angular.z = 0.5;
+  }
 
-	pub->publish(cmd);
+  pub->publish(cmd);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	rclcpp::init(argc, argv);
-	node = std::make_shared<rclcpp::Node>("control_node");
+  rclcpp::init(argc, argv);
+  node = std::make_shared<rclcpp::Node>("control_node");
 
-	auto laser_sub = node->create_subscription<sensor_msgs::msg::LaserScan>(
-		"base_scan", 10,
-		[](const sensor_msgs::msg::LaserScan::SharedPtr msg) { laserCallback(msg); });
-	auto pose_sub = node->create_subscription<nav_msgs::msg::Odometry>(
-		"base_pose_ground_truth", 10,
-		[](const nav_msgs::msg::Odometry::SharedPtr msg) { poseCallback(msg); });
+  auto laser_sub = node->create_subscription<sensor_msgs::msg::LaserScan>(
+    "base_scan", 10,
+    [](const sensor_msgs::msg::LaserScan::SharedPtr msg) { laserCallback(msg); });
+  auto pose_sub = node->create_subscription<nav_msgs::msg::Odometry>(
+    "base_pose_ground_truth", 10,
+    [](const nav_msgs::msg::Odometry::SharedPtr msg) { poseCallback(msg); });
 
 
-	auto timer = node->create_wall_timer(100ms, timerCallback);
-	pub = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto timer = node->create_wall_timer(100ms, timerCallback);
+  pub = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
-	rclcpp::spin(node);
-	rclcpp::shutdown();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
 
-	return 0;
+  return 0;
 }
